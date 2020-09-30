3a2079c1180f875a1c66287bafd3ade3345a5200
NeitorPerup <60506072+NeitorPerup@users.noreply.github.com>
2020-09-30 18:00:20 +0400

    lab 2

---
+++ WindowsFormsWarships/WindowsFormsWarships/FormShip.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsWarships
+{
+    public class FormShip : Form
+    {
+        private PictureBox pictureBox;
+
+        private Button buttonCreateWarship;
+
+        private Button buttonCreateLinkor;
+
+        private Button buttonUp;
+
+        private Button buttonRight;
+
+        private Button buttonDown;
+
+        private Button buttonLeft;
+
+        private ITransport ship;
+        
+        public FormShip()
+        {
+            InitializeComponent();
+        }
+        
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            ship.DrawTransport(gr);
+            pictureBox.Image = bmp;
+        }
+        
+        private void buttonCreateShip_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            ship = new Warship(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Blue);
+            ship.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBox.Width,
+                pictureBox.Height);
+            Draw();
+        }
+        
+        private void buttonCreateSportLinkor_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            ship = new Linkor(rnd.Next(100, 200), rnd.Next(1000, 2000), Color.Blue,
+            Color.Red, true, true, true);
+            ship.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBox.Width, pictureBox.Height);
+            Draw();
+        }
+        
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            //получаем имя кнопки
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "buttonUp":
+                    ship.MoveTransport(Direction.Up);
+                    break;
+                case "buttonDown":
+                    ship.MoveTransport(Direction.Down);
+                    break;
+                case "buttonLeft":
+                    ship.MoveTransport(Direction.Left);
+                    break;
+                case "buttonRight":
+                    ship.MoveTransport(Direction.Right);
+                    break;
+            }
+            Draw();
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormShip));
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.buttonCreateWarship = new System.Windows.Forms.Button();
+            this.buttonCreateLinkor = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(692, 406);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // buttonCreateWarship
+            // 
+            this.buttonCreateWarship.Location = new System.Drawing.Point(0, 0);
+            this.buttonCreateWarship.Name = "buttonCreateWarship";
+            this.buttonCreateWarship.Size = new System.Drawing.Size(69, 40);
+            this.buttonCreateWarship.TabIndex = 1;
+            this.buttonCreateWarship.Text = "Корабль";
+            this.buttonCreateWarship.UseVisualStyleBackColor = true;
+            this.buttonCreateWarship.Click += new System.EventHandler(this.buttonCreateShip_Click);
+            // 
+            // buttonCreateLinkor
+            // 
+            this.buttonCreateLinkor.Location = new System.Drawing.Point(102, 0);
+            this.buttonCreateLinkor.Name = "buttonCreateLinkor";
+            this.buttonCreateLinkor.Size = new System.Drawing.Size(75, 40);
+            this.buttonCreateLinkor.TabIndex = 2;
+            this.buttonCreateLinkor.Text = "Военный корабль";
+            this.buttonCreateLinkor.UseVisualStyleBackColor = true;
+            this.buttonCreateLinkor.Click += new System.EventHandler(this.buttonCreateSportLinkor_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonUp.BackgroundImage")));
+            this.buttonUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonUp.Location = new System.Drawing.Point(636, 349);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(30, 30);
+            this.buttonUp.TabIndex = 3;
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRight.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonRight.BackgroundImage")));
+            this.buttonRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonRight.Location = new System.Drawing.Point(662, 376);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(30, 30);
+            this.buttonRight.TabIndex = 4;
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDown.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonDown.BackgroundImage")));
+            this.buttonDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDown.Location = new System.Drawing.Point(636, 376);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(30, 30);
+            this.buttonDown.TabIndex = 5;
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLeft.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonLeft.BackgroundImage")));
+            this.buttonLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonLeft.Location = new System.Drawing.Point(609, 376);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(30, 30);
+            this.buttonLeft.TabIndex = 6;
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // FormShip
+            // 
+            this.ClientSize = new System.Drawing.Size(692, 406);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.buttonCreateLinkor);
+            this.Controls.Add(this.buttonCreateWarship);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "FormShip";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ WindowsFormsWarships/WindowsFormsWarships/Linkor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WindowsFormsWarships
+{
+    public class Linkor : Warship
+    {        
+        public Color DopColor { private set; get; }
+        
+        public bool Antenna { private set; get; }
+        
+        public bool Cannon { private set; get; }
+
+        public bool Tube { private set; get; }
+
+        public Linkor(int maxSpeed, float weight, Color mainColor, Color dopColor,
+            bool antenna, bool cannon, bool tube) :
+            base(maxSpeed, weight, mainColor, 100, 60)
+        {
+            DopColor = dopColor;
+            Antenna = antenna;
+            Cannon = cannon;
+            Tube = tube;        
+        }
+
+        public override void DrawTransport(Graphics g)
+        {
+            base.DrawTransport(g);
+            Pen pen = new Pen(Color.Black);
+            Brush brDopColor = new SolidBrush(DopColor);
+            if (Tube)
+            {
+                PointF[] tubePoints = {new PointF(_startPosX + 115, _startPosY + 5), new PointF(_startPosX + 108, _startPosY - 12),
+                             new PointF(_startPosX + 126, _startPosY - 18), new PointF(_startPosX + 133, _startPosY + 5) };
+                g.FillPolygon(brDopColor, tubePoints);
+            }
+
+            if (Antenna)
+            {
+                g.FillRectangle(brDopColor, _startPosX + 70, _startPosY, 15, 7);
+                g.FillRectangle(brDopColor, _startPosX + 81, _startPosY - 15, 2, 15);
+            }
+
+            if (Cannon)
+            {
+                PointF[] cannon1Points = { new PointF(_startPosX + 165, _startPosY + 5), new PointF(_startPosX + 170, _startPosY - 7),
+                                           new PointF(_startPosX + 185, _startPosY - 7), new PointF(_startPosX + 190, _startPosY + 5) };
+                g.FillPolygon(brDopColor, cannon1Points);
+                g.FillRectangle(brDopColor, _startPosX + 180, _startPosY - 4, 20, 4);
+
+                PointF[] cannon2Points = { new PointF(_startPosX + 60, _startPosY + 5), new PointF(_startPosX + 55, _startPosY - 7),
+                                           new PointF(_startPosX + 45, _startPosY - 7), new PointF(_startPosX + 40, _startPosY + 5) };
+                g.FillPolygon(brDopColor, cannon2Points);
+                g.FillRectangle(brDopColor, _startPosX + 24, _startPosY - 4, 20, 4);
+            }
+        }
+    }
+}
