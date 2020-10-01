821363c8be0936760362db9e4230dc0f83af52fb
NeitorPerup <60506072+NeitorPerup@users.noreply.github.com>
2020-10-01 14:51:47 +0400

    lab 2

---
+++ FormShip/FormShip/Vehicle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace FormShip
+{
+    public abstract class Vehicle : ITransport
+    {
+        protected float _startPosX;
+
+        protected float _startPosY;
+
+        protected int _pictureWidth;
+
+        protected int _pictureHeight;
+
+        protected readonly int WarshipHeightDown = 31;
+
+        protected readonly int WarshipHeightUp = 15;
+
+        protected readonly int WarshipWidth = 220;
+
+        public int MaxSpeed { protected set; get; }
+
+        public float Weight { protected set; get; }
+
+        public Color MainColor { protected set; get; }
+
+        public void SetPosition(int x, int y, int width, int height)
+        {
+            _startPosX = x;
+            _startPosY = y;
+            _pictureWidth = width;
+            _pictureHeight = height;
+        }
+        public abstract void DrawTransport(Graphics g);
+
+        public abstract void MoveTransport(Direction direction);
+    }
+}
+
